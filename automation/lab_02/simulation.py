69dafe8542e7a292b7fbf6a5e77269f78f34da42
Leonardo Achá <leonardoacha@hotmail.com>
2023-10-09 07:47:54 -0400

    simulation od diff robot in progress

--- automation/lab_02/simulation.py
+++ automation/lab_02/simulation.py
@@ -16,3 +16,27 @@ class Envir:
         #windows settings
         pygame.display.set_caption("Differential drive robot")
         self.map=pygame.display.set_mode((self.width, self.height))
+
+class Robot:
+    def __init__(self, startpos, robotImg, width):
+        self.m2p=3779.52 # meters 2 pixels
+        #robot dims
+        self.w=width
+        self.x=startpos[0]
+        self.y=startpos[1]
+        self.theta=0
+        self.vl=0.01*self.m2p #meters/s
+        self.vr=0.01*self.m2p
+
+if __name__=="__main__":
+    pygame.init()
+    start=(200, 200) #start position
+    dims=(600, 1200)
+    running=True
+    environment=Envir(dims)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running=False
+        pygame.display.update()
+        environment.map.fill(environment.black)
