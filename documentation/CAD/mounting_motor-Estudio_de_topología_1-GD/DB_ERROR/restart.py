20b977ea978c0407c4b7c011a36c2fcf465375bb
Leonardo Achá <leonardoacha@hotmail.com>
2023-10-09 06:19:07 -0400

    cad model and pcb prototype added

---
+++ documentation/CAD/mounting_motor-Estudio_de_topología_1-GD/DB_ERROR/restart.py
@@ -0,0 +1,19 @@
+
+import os
+from SMATsoEnum import *
+
+ParFile = r'mounting_motor-Estudio_de_topología_1-GD.par'
+LogFile = r'TOSCA.OUT'
+LogLevel = LogLevels.INFO
+Overwrite = False
+Report = {PostTypes.SMOOTH}
+FeSolver = FeSolvers.COSMOS
+FeSolverVersion = r''
+inputExt = r'NOD'
+LifeSolver = None
+LifeSolverVersion = r''
+EmagSolver = None
+EnagSolverVersion = r''
+Cpus = 1
+SolCpus = 1
+SolQues = 1
